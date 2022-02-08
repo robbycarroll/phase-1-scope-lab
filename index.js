2338a7180877201e6355ca655ee12dc316a473c1
Robert Carroll <robertcarrollsales@gmail.com>
2022-02-08 13:07:36 -0500

    commit tree e84129eb8a338310116bb8826305ceed5a3e25b1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer 
+const leastFavoriteCustomer = 'poop'
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer;
+}
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase()
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'something'
+}
